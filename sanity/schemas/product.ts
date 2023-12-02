2742117b8325b555dc29f889fd85b5d01772a0d7
Calstins <opulecalebtins@gmail.com>
2023-12-02 18:54:29 +0100

    create environment installed sanity shadce-ui and product schema

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,34 @@
+// schemas/product.ts
+export default {
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name of Product',
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Product images',
+      of: [{type: 'image'}],
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description of product',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Product Slug',
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Product price',
+    },
+  ],
+}
